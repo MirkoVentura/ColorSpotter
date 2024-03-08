b677e1c1728048113a8dea9df90273e2a9813635
Mirko Ventura <mirko.ventura87@gmail.com>
2024-03-08 19:00:48 +0100

    Initial Commit

---
+++ ColorSpotter/ColorSpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSpotterApp.swift
+//  ColorSpotter
+//
+//  Created by Mirko Ventura on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorSpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
